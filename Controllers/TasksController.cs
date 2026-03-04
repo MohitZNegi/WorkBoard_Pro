696f572d1071f8b6fced866d769dcc46a5857f00
MohitZNegi <mohit26negi@gmail.com>
2026-03-04 23:02:34 +1300

    integrated domain models with EF Core via AppDbContext
    - Registered User and TaskItem DbSets
    - Integrated EF Core with OOP-based domain models

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,6 @@
+﻿namespace WorkBoard_Pro.Controllers
+{
+    public class TasksController
+    {
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkBoard_Pro.Models;
+
+namespace WorkBoard_Pro.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
