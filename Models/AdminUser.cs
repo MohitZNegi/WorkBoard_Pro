1234fb891692ac56a286543a6c456deaa555c7f6
MohitZNegi <mohit26negi@gmail.com>
2026-03-04 23:01:00 +1300

    add AdminUser subclass with overridden task permissions
    - Created AdminUser class inheriting from User
    - Overrode CanCompleteTask() to grant full permissions
    - Demonstrated runtime polymorphism through method overriding
    - Applied inheritance to extend domain behavior cleanly

---
+++ Models/AdminUser.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkBoard_Pro.Models
+{
+    public class AdminUser : User
+    {
+        public AdminUser(string name) : base(name) { }
+
+        public override bool CanCompleteTask(TaskItem task)
+        {
+            return true;
+        }
+    }
+}
