5236384ad0d93fe25b1833f8395cf2444c8443b1
MohitZNegi <mohit26negi@gmail.com>
2026-03-04 22:57:34 +1300

    add TaskItem domain model with encapsulated behavior
    - Created TaskItem entity with private setters to enforce invariants
    - Introduced constructor-based initialization for required data
    - Encapsulated state change logic using MarkCompleted method
    - Added User relationship to support ownership and domain modeling

---
+++ Models/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿namespace WorkBoard_Pro.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; private set; }
+
+        public bool IsCompleted { get; private set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        protected TaskItem() { }
+
+        public TaskItem(string title)
+        {
+            Title = title;
+        }
+
+        public void MarkCompleted()
+        {
+            IsCompleted = true;
+        }
+    }
+}
