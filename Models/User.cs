65df31d478f6260e9abe26e3b61301266b7bd0ef
MohitZNegi <mohit26negi@gmail.com>
2026-03-04 22:59:40 +1300

    add User entity with domain-level task validation
    - Created User entity with encapsulated Name property
    - Added one-to-many relationship with TaskItem
    - Implemented constructor-based initialization
    - Introduced domain rule method CanCompleteTask()

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkBoard_Pro.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; private set; }
+
+        public List<TaskItem> Tasks { get; set; } = new();
+
+        protected User() { }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public virtual bool CanCompleteTask(TaskItem task)
+        {
+            return task.UserId == Id;
+        }
+    }
+}
